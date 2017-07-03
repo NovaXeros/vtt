51c0a74b4be4892f692ca7656b80b3be02e3ac2e
Glen Jolley <novaxeros@gmail.com>
2017-07-03 14:23:16 +0100

    Add carrier dropdown and button press feedback

---
+++ app/js/carriers.js
@@ -0,0 +1,178 @@
+var carriers_var = [
+ {
+   "ID": 1,
+   "Name": "Please select"
+ },
+ {
+   "ID": 2,
+   "Name": "None"
+ },
+ {
+   "ID": 3,
+   "Name": "Ageas"
+ },
+ {
+   "ID": 4,
+   "Name": "AIG (Europe) Ltd"
+ },
+ {
+   "ID": 5,
+   "Name": "Allianz"
+ },
+ {
+   "ID": 6,
+   "Name": "Amlin"
+ },
+ {
+   "ID": 7,
+   "Name": "Aviva"
+ },
+ {
+   "ID": 8,
+   "Name": "AXA"
+ },
+ {
+   "ID": 9,
+   "Name": "AXA NSI"
+ },
+ {
+   "ID": 10,
+   "Name": "Beazley"
+ },
+ {
+   "ID": 11,
+   "Name": "Bluesure"
+ },
+ {
+   "ID": 12,
+   "Name": "Canopius"
+ },
+ {
+   "ID": 13,
+   "Name": "Congregational & General"
+ },
+ {
+   "ID": 14,
+   "Name": "DP Mann"
+ },
+ {
+   "ID": 15,
+   "Name": "Ecclesiastical"
+ },
+ {
+   "ID": 16,
+   "Name": "Equity Red Star"
+ },
+ {
+   "ID": 17,
+   "Name": "Faraday"
+ },
+ {
+   "ID": 18,
+   "Name": "Groupama"
+ },
+ {
+   "ID": 19,
+   "Name": "LBG"
+ },
+ {
+   "ID": 20,
+   "Name": "Legal & General"
+ },
+ {
+   "ID": 21,
+   "Name": "Link"
+ },
+ {
+   "ID": 22,
+   "Name": "Midas"
+ },
+ {
+   "ID": 23,
+   "Name": "MMA Insurance"
+ },
+ {
+   "ID": 24,
+   "Name": "Novae"
+ },
+ {
+   "ID": 25,
+   "Name": "PEN Affinity"
+ },
+ {
+   "ID": 26,
+   "Name": "PEN Affinity - Ageas"
+ },
+ {
+   "ID": 27,
+   "Name": "PEN Affinity - Ageas Consortium"
+ },
+ {
+   "ID": 28,
+   "Name": "PEN Affinity - Canopius"
+ },
+ {
+   "ID": 29,
+   "Name": "PEN Affinity - Novae"
+ },
+ {
+   "ID": 30,
+   "Name": "PEN Affinity - RSA"
+ },
+ {
+   "ID": 31,
+   "Name": "PEN e-underwriting - Ageas"
+ },
+ {
+   "ID": 32,
+   "Name": "PEN e-underwriting - Aviva"
+ },
+ {
+   "ID": 33,
+   "Name": "PEN e-underwriting - L&G"
+ },
+ {
+   "ID": 34,
+   "Name": "PEN e-underwriting - RSA"
+ },
+ {
+   "ID": 35,
+   "Name": "PEN Novae"
+ },
+ {
+   "ID": 36,
+   "Name": "PEN Personal Lines RSA"
+ },
+ {
+   "ID": 37,
+   "Name": "PEN Woodbrook - RSA"
+ },
+ {
+   "ID": 38,
+   "Name": "Prestige"
+ },
+ {
+   "ID": 39,
+   "Name": "Prestige Bluesure"
+ },
+ {
+   "ID": 40,
+   "Name": "Prestige Home"
+ },
+ {
+   "ID": 41,
+   "Name": "Prestige NSI"
+ },
+ {
+   "ID": 42,
+   "Name": "RSA"
+ },
+ {
+   "ID": 43,
+   "Name": "Towergate"
+ },
+ {
+   "ID": 44,
+   "Name": "Zurich"
+ }
+];
