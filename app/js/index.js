fdc26844dab28e581a8187e70a9095376a55175a
Glen Jolley <novaxeros@gmail.com>
2017-07-03 15:16:32 +0100

    Add carrier dropdown and button press feedback

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,20 @@
+//setup event listener and ipc communication for app.close button press
 const ipc = require('electron').ipcRenderer
 
 var closeEl = document.getElementById('close-button');
 
 closeEl.addEventListener('click', function() {
     ipc.send('close-main-window');
+});
+
+//get a reference to the select element
+var $select = $('#carrier');
+ 
+//clear the current content of the select
+$select.html('');
+ 
+//iterate over the data and append a select option
+
+$.each(carriers_var, function(key, val){ 
+$select.append('<option id="' + val.ID + '">' + val.Name + '</option>');
 });
